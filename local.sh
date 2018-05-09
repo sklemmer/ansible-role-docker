48933eb6f96777fbe9aa0b576c5feeabd1552827
sven klemmer <svenklemmer@aol.com>
2018-05-10 00:17:51 +0200

    add update true and remove old tests

--- local.sh
+++ local.sh
@@ -3,7 +3,7 @@
 #docker run -it -v $PWD:/workspace -w /workspace python:2.7-slim /bin/bash
 
 apt update
-apt install ruby-dev curl
+apt install -y ruby-dev curl
 
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 
